7bb4bfa8e37b4753a6ea676b474532ef1b2d3de6
afterhours88 <bonifacino.andrea@gmail.com>
2013-08-02 18:06:44 +0200

    commit_1 02/08/2013
    prove del cazzo

--- Unity/Assets/CommunicationWithArduino.cs
+++ Unity/Assets/CommunicationWithArduino.cs
@@ -39,6 +39,16 @@ public class CommunicationWithArduino : MonoBehaviour {
 			try{
 				string str = sp.ReadLine();
 				accelerometer = str.Split(';');
+				//if (int.Parse(accelerometer[0])>45)
+				//	accelerometer[0]="45";
+				
+				for(int i=0; i<3; i++)
+				{
+					if (int.Parse(accelerometer[i])>45)
+						accelerometer[i]="45";
+					else if (int.Parse(accelerometer[i])<-45)
+						accelerometer[i]="-45";
+				}
 				//Debug.Log(str);
 				MoveThePlane(accelerometer);
 			}
@@ -49,6 +59,7 @@ public class CommunicationWithArduino : MonoBehaviour {
 	}
 	
 	void MoveThePlane (string[] accelerometer){
+		
 		Quaternion target = Quaternion.Euler(-(float.Parse(accelerometer[0])),0,float.Parse(accelerometer[1]));
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 2.0f);
 	//	transform.Rotate(-(int.Parse(accelerometer[0])-temp[0]),0,int.Parse(accelerometer[1])-temp[1],Space.Self);
