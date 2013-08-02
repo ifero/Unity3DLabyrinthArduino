f0fc4b99e13f6cd90d47ca9a8a3959234e56565b
Andrea Pacino <andrea.pacino87@gmail.com>
2013-08-02 16:13:45 +0200

    inizializzazione progetto

---
+++ Arduino/codice_arduino/codice_arduino.ino
@@ -0,0 +1,104 @@
+#include <Wire.h>
+
+#define LIS302_POWER_PIN 13
+#define LIS302_INT2_PIN 2
+
+#define I2C_ADDRESS 28
+#define POWER_UP_DELAY 10
+#define REGISTER_WHO_AM_I 0x0F
+#define WHO_AM_I_RESULT 0x3B
+#define CTRL_REG1 0x20
+#define CTRL_XEN 0
+#define CTRL_YEN 1
+#define CTRL_ZEN 2
+#define CTRL_STM 3
+#define CTRL_STP 4
+#define CTRL_FS 5
+#define CTRL_PD 6
+#define CTRL_DR 7
+
+#define X_OUT 0x29
+#define Y_OUT 0x2b
+#define Z_OUT 0x2d
+
+void setup()
+{  
+  Serial.begin(115200);  // start serial for output
+  setupLIS();
+  startLIS();
+}
+
+void loop()
+{
+  Serial.flush(); // Empty the memory each time in the loop
+  char x = readRegister(X_OUT);
+  char y = readRegister(Y_OUT);
+  char z = readRegister(Z_OUT);
+  
+  //char xmap = map(x,60,-60,90,-90);
+  //char ymap = map(y,60,-60,90,-90);
+  //char zmap = map(z,60,-60,90,-90);
+  Serial.print(x,DEC);
+  Serial.print(";");
+  Serial.print(y,DEC);
+  Serial.print(";");
+  Serial.print(z,DEC);
+  Serial.println(";");
+
+  delay(33);
+}
+
+void setupLIS() {
+  
+  Serial.println("Setting up LIS302DL");
+  
+  pinMode(LIS302_POWER_PIN, OUTPUT); //the power pin
+  pinMode(LIS302_INT2_PIN, INPUT); //the int2 pin
+  
+  digitalWrite(LIS302_POWER_PIN,LOW); //switch off the LIS302
+  
+  Wire.begin();        // join i2c bus (address optional for master)
+  
+}
+
+void startLIS() {
+  
+  Serial.println("Powering up LIS302DL");
+  digitalWrite(LIS302_POWER_PIN,HIGH); //switch off the LIS302
+
+  delay(POWER_UP_DELAY);
+  writeRegister(CTRL_REG1 , _BV(CTRL_PD) | _BV(CTRL_XEN) | _BV(CTRL_YEN) | _BV(CTRL_ZEN));
+  
+  //read who am i
+  char myself = readRegister(REGISTER_WHO_AM_I);
+  if (myself == WHO_AM_I_RESULT) {
+    Serial.println("yippie got an lis302DL!");
+  } else {
+    Serial.print("found an ");
+    Serial.print(myself, HEX);
+    Serial.println("! very strange!");
+  }
+}
+
+void writeRegister(unsigned char r, unsigned char v)
+{
+  Wire.beginTransmission(I2C_ADDRESS);
+  Wire.write(r);
+  Wire.write(v);
+  Wire.endTransmission();
+}
+
+unsigned char readRegister(unsigned char r)
+{
+  unsigned char v;
+  Wire.beginTransmission(I2C_ADDRESS);
+  Wire.write(r);  // register to read
+  Wire.endTransmission();
+
+  Wire.requestFrom(I2C_ADDRESS, 1); // read a byte
+  while(!Wire.available()) {
+    // waiting
+  }
+  v = Wire.read();
+  return v;
+}
---
+++ Unity/Assets/CommunicationWithArduino.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class CommunicationWithArduino : MonoBehaviour {
+	float[] temp;
+	//Windows Bonifacino
+	SerialPort sp = new SerialPort("COM9",115200); 
+	
+	//Windows Pacino
+	//SerialPort sp = new SerialPort("COM9",115200); 
+	
+	//Mac
+	//SerialPort sp = new SerialPort("5331",115200);
+	string[] accelerometer;
+	
+	// Use this for initialization
+	void Start () {
+		sp.Open();
+		if(sp.IsOpen){
+			try{
+				string str = sp.ReadLine();
+				accelerometer = str.Split(';');
+				//Debug.Log(str);
+				temp[0]=float.Parse(accelerometer[0]);
+				temp[1]=float.Parse(accelerometer[1]);
+				//sp.BaseStream.Flush();
+			}
+			catch(System.Exception){
+				
+			}
+		}
+		sp.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(sp.IsOpen){
+			try{
+				string str = sp.ReadLine();
+				accelerometer = str.Split(';');
+				//Debug.Log(str);
+				MoveThePlane(accelerometer);
+			}
+			catch(System.Exception){
+				
+			}
+		}
+	}
+	
+	void MoveThePlane (string[] accelerometer){
+		Quaternion target = Quaternion.Euler(-(float.Parse(accelerometer[0])),0,float.Parse(accelerometer[1]));
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 2.0f);
+	//	transform.Rotate(-(int.Parse(accelerometer[0])-temp[0]),0,int.Parse(accelerometer[1])-temp[1],Space.Self);
+	//	temp[0]=int.Parse(accelerometer[0]);
+	//	temp[1]=int.Parse(accelerometer[1]);
+		//sp.BaseStream.Flush();
+		//Debug.Log(accelerometer[0]+" - "+accelerometer[1]+" - "+accelerometer[2]);
+	}
+}
