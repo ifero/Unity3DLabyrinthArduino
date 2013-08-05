71a32c248319b547dc74f45e3dff9418b29f40c6
afterhours88 <bonifacino.andrea@gmail.com>
2013-08-05 18:27:32 +0200

    creati script per il timer
    non ancora funzionante al 100%

---
+++ Unity/Assets/Timer.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+private var startTime:float;
+var textTime:String;
+var Flag:boolean;
+
+function Start () {
+	startTime=Time.time;
+	Flag=true;
+}
+
+function Update () {
+	
+	if (Flag)
+	{
+		var guitime=Time.time-startTime;
+		var minutes = guitime/60;
+		var seconds = guitime%60;
+		var fraction = (guitime *100)%100;
+		
+		textTime=String.Format("{0:00}:{1:00}:{2:00}",minutes,seconds,fraction);
+		GetComponent(GUIText).text = textTime;
+	}
+}
---
+++ Unity/Assets/Trigger_script.js
@@ -0,0 +1,9 @@
+﻿function OnTriggerEnter() {
+	//oggettoPalla = GameObject.Find("Palla");
+	//componenteValoredado = oggettoDado.GetComponent("ValoreDado");
+	//componenteValoredado.valoreCorrente = valoreFaccia;
+	//Debug.Log(valoreFaccia);
+	objGuiText = GameObject.Find("Testo");
+	componenteTimer = objGuiText.GetComponent("Timer");
+	componenteTimer.Flag=false;
+}
