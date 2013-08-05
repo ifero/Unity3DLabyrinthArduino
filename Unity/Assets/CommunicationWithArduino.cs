2240ceae573f6c9dd8697739d29d31ec76c2abb9
Andrea Pacino <andrea.pacino87@gmail.com>
2013-08-05 16:08:25 +0200

    Rimosso Codice inutile
    Rimossi i temp, inutilizzati

--- Unity/Assets/CommunicationWithArduino.cs
+++ Unity/Assets/CommunicationWithArduino.cs
@@ -2,16 +2,9 @@
 using System.Collections;
 using System.IO.Ports;
 
+
 public class CommunicationWithArduino : MonoBehaviour {
-	float[] temp;
-	//Windows Bonifacino
-	SerialPort sp = new SerialPort("COM9",115200); 
-	
-	//Windows Pacino
-	//SerialPort sp = new SerialPort("COM9",115200); 
-	
-	//Mac
-	//SerialPort sp = new SerialPort("5331",115200);
+	SerialPort sp = new SerialPort("COM3",115200); 
 	string[] accelerometer;
 	
 	// Use this for initialization
@@ -22,8 +15,6 @@ public class CommunicationWithArduino : MonoBehaviour {
 				string str = sp.ReadLine();
 				accelerometer = str.Split(';');
 				//Debug.Log(str);
-				temp[0]=float.Parse(accelerometer[0]);
-				temp[1]=float.Parse(accelerometer[1]);
 				//sp.BaseStream.Flush();
 			}
 			catch(System.Exception){
@@ -60,11 +51,9 @@ public class CommunicationWithArduino : MonoBehaviour {
 	
 	void MoveThePlane (string[] accelerometer){
 		
-		Quaternion target = Quaternion.Euler(-(float.Parse(accelerometer[0])),0,float.Parse(accelerometer[1]));
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 2.0f);
-	//	transform.Rotate(-(int.Parse(accelerometer[0])-temp[0]),0,int.Parse(accelerometer[1])-temp[1],Space.Self);
-	//	temp[0]=int.Parse(accelerometer[0]);
-	//	temp[1]=int.Parse(accelerometer[1]);
+			Quaternion target = Quaternion.Euler(-(float.Parse(accelerometer[0])),0,float.Parse(accelerometer[1]));
+        	transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 2.0f);
+		//	transform.Rotate(-(int.Parse(accelerometer[0])-temp[0]),0,int.Parse(accelerometer[1])-temp[1],Space.Self);
 		//sp.BaseStream.Flush();
 		//Debug.Log(accelerometer[0]+" - "+accelerometer[1]+" - "+accelerometer[2]);
 	}
