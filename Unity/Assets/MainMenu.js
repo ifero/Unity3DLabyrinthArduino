d48ed0a4deb6e4afb636cc5bdb74566a83358066
Andrea Pacino <andrea.pacino87@gmail.com>
2013-08-12 15:55:52 +0200

    Elimina HighScores
    Aggiunti i bottoni per eliminare gli highscores e per tornare al menu
    principale

--- Unity/Assets/MainMenu.js
+++ Unity/Assets/MainMenu.js
@@ -1,4 +1,6 @@
-﻿var mainMenu = true;
+﻿import System.IO;
+
+var mainMenu = true;
 var highScoreMenu = false;
 
 function OnGUI()
@@ -17,7 +19,7 @@ function OnGUI()
 	if(highScoreMenu){
 		var sr : StreamReader = new StreamReader("Assets/HighScores.txt");
 		var i = 0;
-		var offset = 200;
+		var offset = 150;
 		var lenght = sr.ReadToEnd();
 		lines = lenght.Split("\n"[0]);
 		if(lines.length != 1 || lines[0] != ""){
@@ -34,6 +36,15 @@ function OnGUI()
 			GUI.Box(Rect(Screen.width/2 - 50,offset,170,50),"00:00:00 unknown");
 			i++;
 			offset+=50;	
+		}
+		if (GUI.Button(Rect(Screen.width/2+150,170,150,50),"Cancella High Scores")){
+			var sw : StreamWriter = new StreamWriter("Assets/HighScores.txt");
+			sw.Write(null);
+			sw.Close();
+		}
+		if (GUI.Button(Rect(Screen.width/2+150,230,150,50),"Main Menu")){
+			highScoreMenu = false;
+			mainMenu = true;
 		}
 	}
 }
