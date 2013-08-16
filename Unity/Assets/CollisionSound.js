dcefb2ad4e896afb53673360747a438d094b47c0
afterhours88 <bonifacino.andrea@gmail.com>
2013-08-16 18:19:07 +0200

    Aggiunta suono collisione; script imprimere forza
    Il .waw è da cambiare. Lo script sembra non funzionare.

--- Unity/Assets/CollisionSound.js
+++ Unity/Assets/CollisionSound.js
@@ -1,8 +1,7 @@
 ﻿// Check if the collision is with the gameObject "muro". If is it so, set the flag hitTheWall TRUE
 function OnCollisionEnter(collision:Collision){
-	if (collision.gameObject.name == "muro") {
-		
+	if (collision.gameObject.name != "Pavimento") {
 			GameObject.Find("Labirinto").GetComponent("CommunicationWithArduino").hitTheWall = true;
-	
+			transform.audio.Play();
 		}
 }
---
+++ Unity/Assets/script_force.js
@@ -0,0 +1,6 @@
+﻿private var v3Normal : Vector3;
+function OnCollisionEnter (collision:Collision){
+	v3Normal = collision.contacts[0].normal;
+	objRigidbody = collision.gameObject.GetComponent("Rigidbody");
+	objRigidbody.AddRelativeForce(v3Normal*3000,ForceMode.Acceleration);
+}
